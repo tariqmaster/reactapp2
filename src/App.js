c21cd9dfb9b9829e26b41b3814f0292a44351acc
tariqmaster <tariqchemengr@gmail.com>
2020-06-10 00:39:47 +0500

    again

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        I am tariq updating react app.
-        <p>
+        <p>Hello! I am tariq updating react app.
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
